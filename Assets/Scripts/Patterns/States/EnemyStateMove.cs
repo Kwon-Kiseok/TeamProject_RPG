49202cb7ac46a28398ee7344076a2489fcbda3c0
Justpush <a85429462@gmail.com>
2022-07-19 08:22:48 +0900

    [Update] [2022.07.19 박지용] : Create Sword Monster, HandMonster
    소드몬스터 , 권투몬스터 구현

--- Assets/Scripts/Patterns/States/EnemyStateMove.cs
+++ Assets/Scripts/Patterns/States/EnemyStateMove.cs
@@ -4,29 +4,29 @@ using HOGUS.Scripts.Enums;
 using UnityEngine;
 namespace HOGUS.Scripts.State
 {
-    //public class EnemyStateMove : IState
-    //{
-    //    private MonsterController enemy;
-    //    public EnemyStateMove(MonsterController e)
-    //    {
-    //        enemy = e;
-    //    }
-    //    public void StateEnter()
-    //    {
+    public class EnemyStateMove : IState
+    {
+        private MonsterController enemy;
+        public EnemyStateMove(MonsterController e)
+        {
+            enemy = e;
+        }
+        public void StateEnter()
+        {
 
-    //    }
-    //    public void StateUpdate()
-    //    {
-            
-    //    }
-    //    public void StateExit()
-    //    {
+        }
+        public void StateUpdate()
+        {
 
-    //    }
+        }
+        public void StateExit()
+        {
 
-    //    public void StateFixedUpdate()
-    //    {
+        }
 
-    //    }
-    //}
+        public void StateFixedUpdate()
+        {
+
+        }
+    }
 }
