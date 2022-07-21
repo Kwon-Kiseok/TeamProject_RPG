99f4aa19a6bfc45f25caa4118135f4878e2c1fcc
DESKTOP-CMU00NV\JHS <ggg200005@gmail.com>
2022-07-21 11:54:13 +0900

    [UPDATE][2022-07-21 진현섭] QUEST_Achievement And Reward

---
+++ Assets/Scripts/UI/Quest/Infrun_Task_Script/Condition/Condition.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Condition : ScriptableObject
+{
+    [SerializeField]
+    private string description;
+    public abstract bool IsPass(Quest quest);
+}
