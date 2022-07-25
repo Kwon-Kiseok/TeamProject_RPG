e475bc709aebb0f65eb529c863550f9e67b80cc7
Justpush <a85429462@gmail.com>
2022-07-26 01:13:26 +0900

    [Create] Update bossMonster

---
+++ Assets/JiyongPark/TestEnemyScene3/Animation/Boss/Script/BossProjectTile.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using HOGUS.Scripts.Character;
+
+[RequireComponent(typeof(Rigidbody))]
+public class BossProjectTile : MonoBehaviour
+{
+    private Rigidbody rb;   
+    public int damage;         
+    private NavMeshAgent navi;
+    public Transform target;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
+        navi = GetComponent<NavMeshAgent>();
+        navi.SetDestination(target.position);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player Hit");
+            var player = collision.gameObject.GetComponent<Player>();
+            player.GetCurrentStatus().TakeDamage(damage);       
+            Destroy(gameObject);
+        }
+
+        Debug.Log(collision.gameObject.name + " " + collision.gameObject.tag);
+    }
+}
